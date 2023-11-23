9e1649cce598df037f0a39772efdf9a954be5f3d
Enrique Arranz <enrique.arrantzale@gmail.com>
2023-11-23 10:35:43 +0100

    ACTUALIZACION

--- SPRINT_1_TASCA_8_LAMBDAS/src/n1exercici1/Main.java
+++ SPRINT_1_TASCA_8_LAMBDAS/src/n1exercici1/Main.java
@@ -6,17 +6,18 @@ import java.util.stream.Stream;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-		Stream<String> streamOfStrings = Stream.of("Antonio", "Paco", "Ana", "Julia").filter(s -> s.contains("o"));
-		
-		// .filter(s -> s.startsWith("P"));// FILTRO PARA PRIMERA LETRA DEL STRING
-		// .map (s -> s.toUpperCase()); // UTILIDAD PARA MODIFICAR CONTENIDO
-		// .count(); //NOS DEVUELVE LA CUENTA
-		// .anyMatch(s -> s.contains("o"));// BOOLEANO TIPO EXISTS
-		List<String> lista = streamOfStrings.collect(Collectors.toList());
-		System.out.println(lista);
-	}
+        Stream<String> streamOfStrings = Stream.of("Antonio", "Paco", "Ana", "Julia").filter(s -> s.contains("o"));
+
+        // .filter(s -> s.startsWith("P"));// FILTRO PARA PRIMERA LETRA DEL STRING
+        // .map (s -> s.toUpperCase()); // UTILIDAD PARA MODIFICAR CONTENIDO
+        // .count(); //NOS DEVUELVE LA CUENTA
+        // .anyMatch(s -> s.contains("o"));// BOOLEANO TIPO EXISTS
+        List<String> lista = streamOfStrings.collect(Collectors.toList());
+             System.out.println(lista);
+
+    }
 
 }
